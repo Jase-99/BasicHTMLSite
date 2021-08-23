0e3970d81830065297b9384449ce7cefc720e8a8
Jason Henshall <jasonhenshall@hotmail.com>
2021-08-23 10:59:55 +1000

    added javascript to zoom image gallery, using a jquery plugin

---
+++ script.js
@@ -0,0 +1,10 @@
+mediumZoom('.zoom', {
+    margin: 50,
+    background: 'rgba(0,0,0,0.7)',
+    scrollOffset: 50
+})
+
+function demo () {
+    document.getElementById('demo').style.fontSize="44px"
+}
+
